1233b2a4dd7443867d1e48065ed04d0296daddfe
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-03-17 14:05:24 -0300

    first commit

---
+++ aberta.js
@@ -0,0 +1,19 @@
+/*
+
+O manifesto ágil foi um divisor na forma como os projetos eram desenvolvidos, mudando a maneira de como montamos os times e 
+controlamos os processos, além da valorização e aproximação com o cliente, tanto que hoje é um tema comum nas graduações e cursos.
+
+Você consegue falar um pouco sobre uma das metodologias que surgiram e o que te chama atenção nela?
+
+
+----> Resposta: O método Kanban é um dos métodos mais simples utilizados atualmente, tendo como objetivo buscar a evolução do projeto, permitindo de 
+uma forma bem visual a organização dos processos, sendo dividido da seguinte maneira:
+
+- To Do: são as atividades que precisarão ser realizadas;
+- Doing: são as atividades que estão sendo realizadas no momento;
+- Done: são as atividades finalizadas, que já foram avaliadas e aprovadas pela equipe.
+
+O que mais me chama a atenção é que ele basicamente é composto de checklists, oferecendo uma ótima visão do projeto todo, sendo uma forma 
+de realizar o gerenciamento de projetos e adaptável a qualquer tipo de negócio.
+
+*/
